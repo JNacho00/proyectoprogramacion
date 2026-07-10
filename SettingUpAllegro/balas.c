df6ac5c7262d876201d8cac05f849b0ae256bbca
JNacho00 <jocacere@umagallanes.cl>
2026-07-10 15:25:33 -0300

    corrección balas

--- SettingUpAllegro/balas.c
+++ SettingUpAllegro/balas.c
@@ -1,4 +1,5 @@
 #include "balas.h"
+#include "personaje.h"
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
@@ -8,9 +9,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include "mapa.h"
+#define max_balas_p 3
 
-
-bala balas[max_balas_p];
 
 void dibujar_bala(bala* b, float camara_x, float camara_y) {
 	al_draw_filled_rectangle(
@@ -22,29 +22,78 @@ void dibujar_bala(bala* b, float camara_x, float camara_y) {
 	);
 }
 
-bool crear_bala(int x, int y, float belocidadx, float belocidady) {
-	
-	static int i;
+void spawn_balas(bala balas[]) {
+	int i;
 
+	for (i = 0; i < max_balas_p; i++) {
+		balas[i].x = 0;
+		balas[i].y = 0;
+
+		balas[i].ancho = ancho_bala;
+		balas[i].alto = alto_bala;
+
+		balas[i].velocidad_bx = 0;
+		balas[i].velocidad_by = 0;
+
+		balas[i].activa = false;
+	}
+}
+
+bool crear_bala(personaje* p, bala balas[]) {
+	
+	int i;
+	float vx;
+	float vy;
+	float posx;
+	float posy;
+
+	if (p->municion <= 0) {
+		return;
+	}
+
+	if (p->direccionx == 0 && p->direcciony == 0) {
+		return;
+	}
+
+	vx = p->direccionx * velocidad_bala;
+	vy = p->direcciony * velocidad_bala;
+
+	posx = p->x + (p->ancho - ancho_bala) / 2.0f;
+	posy = p->y + (p->alto - alto_bala) / 2.0f;
+
+	if (p->direccionx > 0) {
+		posx = p->x + p->ancho + 1;
+	}
+	else if (p->direccionx < 0) {
+		posx = p->x - ancho_bala - 1;
+	}
+
+	if (p->direcciony > 0) {
+		posy = p->y + p->alto + 1;
+	}
+	else if (p->direcciony < 0) {
+		posy = p->y - alto_bala - 1;
+	}
 
 	for (i = 0; i < max_balas_p; i++) {
 		if (balas[i].activa == false) {
 
-			balas[i].x = x;
-			balas[i].y = y;
-			balas[i].velocidad_bx = belocidadx;
-			balas[i].velocidad_by = belocidady;
+			balas[i].x = posx;
+			balas[i].y = posy;
+			balas[i].velocidad_bx = vx;
+			balas[i].velocidad_by = vy;
 			balas[i].alto = alto_bala;
 			balas[i].ancho = ancho_bala;
 
 			balas[i].activa = true;
+			p->municion--;
 			return true;
 		}
 	}
 	return false;
 }
 
-void dibujar_balas_mapa(float camara_x, float camara_y) {
+void dibujar_balas_mapa(bala balas[], float camara_x, float camara_y) {
 	int i;
 	for (i = 0; i < max_balas_p; i++) {
 		if (balas[i].activa == true) {
@@ -53,7 +102,7 @@ void dibujar_balas_mapa(float camara_x, float camara_y) {
 	}
 }
 
-void fisicas_balas() {
+void fisicas_balas(bala balas[]) {
 	int i;
 
 	for (i = 0; i < max_balas_p; i++) {
